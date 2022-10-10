335c3f670071532618ba8b4ec54e828021e7bff9
Tiago Gabriel <tiago.gabriel@anchorlabs.com>
2022-10-10 12:57:29 +0000

    refactor: Introduce mux to allow for other methods support

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module poc/onboarding-api
 
 go 1.19
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", welcome)
-	http.HandleFunc("/v1/customers", postCustomers)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	r := mux.NewRouter().StrictSlash(true)
+	configureEndpoints(r)
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
+
+func configureEndpoints(r *mux.Router) {
+	r.HandleFunc("/", welcome)
+	r.HandleFunc("/v1/customers", postCustomers).Methods("POST")
+	r.HandleFunc("/v1/customers/{id}", putCustomer).Methods("PUT")
 }
